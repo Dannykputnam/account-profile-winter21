03d8920aa38a64ac5d6ec76f1ff87409738b2e04
Danny Putnam <dannykputnam@gmail.com>
2021-12-21 20:22:24 -0700

    style completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/shared/Home';
 import Profile from './components/users/Profile';
 import NoMatch from './components/shared/NoMatch';
 import Navbar from './components/shared/Navbar';
+import { Container } from 'react-bootstrap';
 
 const App = () => (
   <>
     <Navbar />
-    <>
+    <Container>
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/profile' element={<Profile />} />
         <Route path='*' element={<NoMatch />} />
       </Routes>
-    </>
+    </Container>
   </>
 )
 
